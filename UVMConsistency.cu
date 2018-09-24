b5c549573f1062a8d5062bdb4337142520c7335a
Ophir Katz <ophirk8396@gmail.com>
2018-09-24 11:40:17 +0300

    removed GPU_FINISH

--- UVMConsistency.cu
+++ UVMConsistency.cu
@@ -99,8 +99,8 @@ __device__ void deposit_to_account(UVMSPACE ManagedBankAccount *bank_account, un
   __threadfence_system();
 
   ONLY_THREAD {
-    *finished = GPU_FINISH;
-    __threadfence_system(); // Writing finished GPU memory so CPU can see
+    // *finished = GPU_FINISH;
+    // __threadfence_system(); // Writing finished GPU memory so CPU can see
 
     // NOTE : need to sync this for the change to be seen in CPU (this is whats being tested)
     atomicAdd((ulli *) &bank_account->balance, (ulli) deposit_amount);
