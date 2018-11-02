28c8ab3b553ae924849b6dd50a769cf8d03f7509
Ophir Katz <ophirk8396@gmail.com>
2018-11-02 11:58:41 +0200

    st compiles

--- UVMConsistency.cu
+++ UVMConsistency.cu
@@ -30,10 +30,14 @@ namespace UVMConsistency {
 typedef unsigned long long int ulli;
 
 __device__ void write_fenced(UVMSPACE int *address, UVMSPACE int *finished) {
-  asm volatile ("add.u32 %0, %1, %2;"
+  // asm volatile ("add.u32 %0, %1, %2;"
+  //     : "=r"  (address[0])
+  //     : "r"   (0),
+  //       "r"   (1)
+  // );
+
+  asm volatile ("st.u32 [%0], 1;"
       : "=r"  (address[0])
-      : "r"   (0),
-        "r"   (1)
   );
 }
 
