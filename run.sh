f2abe673e7a2ef616ad7cacec0a5d8f95472704f
Ophir Katz <ophirk8396@gmail.com>
2018-11-02 14:37:29 +0200

    fixed run.sh option

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-nvcc -g -G UVMConsistency.cu -o consistency.out -Wno-deprecated-gp
-u-targets
+nvcc -g -G UVMConsistency.cu -o consistency.out -Wno-deprecated-gpu-targets
 
 for i in $(seq 1 20); do
   ./consistency.out
