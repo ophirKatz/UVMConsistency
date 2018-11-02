c88fcbdfd5584b3c6549c7d6c44abea53b07a102
Ophir Katz <ophirk8396@gmail.com>
2018-11-02 14:35:56 +0200

    no warnings run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-nvcc -g -G UVMConsistency.cu -o consistency.out
+nvcc -g -G UVMConsistency.cu -o consistency.out -Wno-deprecated-gp
+u-targets
 
 for i in $(seq 1 20); do
   ./consistency.out
